af758f8694644d05fef5036455d8c95acfccef95
spencernewell93 <spencernewell93@gmail.com>
2015-02-05 13:23:25 -0600

    updated Transaction class in models

--- CSCE4925-ITCapstoneII/Models/Transaction.cs
+++ CSCE4925-ITCapstoneII/Models/Transaction.cs
@@ -7,5 +7,16 @@ namespace SQLSolutions.Models
 {
     public class Transaction
     {
+        //id of the transaction {primary key}
+        public virtual int idNum { get; set; }
+        //foreign key of user id 
+        public virtual int user_idNum { get; set; }
+        //foreign key of book id
+        public virtual int book_idNum { get; set; }
+        //date book was checked out
+        public virtual DateTime checkoutDate { get; set; }
+        //date book is due
+        public virtual DateTime dueDate { get; set; }
+
     }
 }
