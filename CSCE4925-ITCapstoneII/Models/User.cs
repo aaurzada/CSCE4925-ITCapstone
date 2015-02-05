cb021e65c61fd69ab0b21bd63ea3c01817910030
spencernewell93 <spencernewell93@gmail.com>
2015-02-05 12:43:39 -0600

    added User class to models

--- CSCE4925-ITCapstoneII/Models/User.cs
+++ CSCE4925-ITCapstoneII/Models/User.cs
@@ -7,7 +7,15 @@ namespace SQLSolutions.Models
 {
     public class User
     {
-        //add variables below
-        
+        //user idNum {primary key}
+        public virtual int idNum { get; set; }
+        //user euid for log in purposes
+        public virtual String euid { get; set; }
+        //first name
+        public virtual String firstName { get; set; }
+        //last name
+        public virtual String lastName { get; set; }
+        //email
+        public virtual String email { get; set; }
     }
 }
