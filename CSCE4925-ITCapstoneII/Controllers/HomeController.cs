d5b309561205885b5015fb1c88e04193e063b516
spencernewell93 <spencernewell93@gmail.com>
2015-02-17 14:33:56 -0600

    updated HomeCrontroller login validation and Login.cs isValid function

--- CSCE4925-ITCapstoneII/Controllers/HomeController.cs
+++ CSCE4925-ITCapstoneII/Controllers/HomeController.cs
@@ -21,13 +21,23 @@ namespace SQLSolutions.Controllers
         {
             if (ModelState.IsValid) //check if input is valid
             {
-                if (Login.IsValid(Login.euid, Login.password))
+                if (Login.IsValid(Login.euid, Login.password) == "admin") //given by view and sent to model IsValid function to check against user table and UNT auth
                 {
-       
-                    return RedirectToAction("Index", "Home");
+                    Session["user"] = Login.euid; //session created 
+                    Session["admin"] = true;
+                    //check if admin and display pages accordingly
+                    return RedirectToAction("Index", "Home"); //non admin
+                }
+                else if (Login.IsValid(Login.euid, Login.password) == "nonAdmin")
+                {
+                    Session["user"] = Login.euid; //session created 
+                    Session["admin"] = false;
+                    //check if admin and display pages accordingly
+                    return RedirectToAction("Index", "Home"); //non admin
                 }
                 else
                 {
+
                     ModelState.AddModelError("", "Login data is incorrect!");
                 }
             }
--- CSCE4925-ITCapstoneII/Models/Login.cs
+++ CSCE4925-ITCapstoneII/Models/Login.cs
@@ -18,21 +18,35 @@ namespace SQLSolutions.Models
         [Display(Name = "Password:")]
         public virtual String password { get; set; } //may delete...dont actually need to store
 
-        public bool IsValid(string _username, string _password)
+        public String IsValid(string _username, string _password)
         {
-            bool accountExists = false;
+            bool accountExists = false; //account exists in user table
+            bool isAdmin = false; //account is admin in user table
             //check if euid is valid in user table
             if (Database.Session.Query<User>().Any(u => u.euid == _username))
-            {
+            { 
+                //check if user is admin in user table
+                isAdmin = true;
                 accountExists = true;
+               
             }
-            //check if euid and password valid in UNT auth
-            if (accountExists == true) //if account exists then check password validity through UNT auth
+            //---check if euid and password valid in UNT auth---
+            if (accountExists == true && isAdmin == true) //if account exists then check password validity through UNT auth
             {
-                //send _username and _password
+                //send _username and _password 
+                //return bool depending if username and password match
+                //if return true then return "admin"
+                return "admin";
             }
-            //else return false;
-            return true; //just for now
+            else if (accountExists == true && isAdmin == false)
+            { 
+                //send _username and _password to UNT auth
+                //return bool depending if username and password match
+                //if return true then return "nonAdmin"
+                return "nonAdmin";
+            }
+            else
+                return "notExists"; //just for now
         }
     }
 }
