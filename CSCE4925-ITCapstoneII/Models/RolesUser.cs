2b249914fc20cc936e949833362dcf8e0ff5a835
Andrew Aurzada <aaurzada@gmail.com>
2015-03-03 14:10:46 -0600

    Added entity mappings for Roles and RolesUser models

---
+++ CSCE4925-ITCapstoneII/Models/RolesUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NHibernate.Mapping;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace SQLSolutions.Models
+{
+    public class RolesUser
+    {
+        public virtual int UserId { get; set; }
+        public virtual int RoleId { get; set; }
+    }
+
+    public class RolesUserMap : ClassMapping<RolesUser>
+    {
+        public RolesUserMap()
+        {
+            Table("roles_user");
+
+            Property(x => x.UserId, x =>
+            {
+                //override column name to match table
+                x.Column("user_id");
+                x.NotNullable(true);
+            });
+            Property(x => x.RoleId, x =>
+            {
+                //override column name to match table
+                x.Column("role_id");
+                x.NotNullable(true);
+            });
+        }
+    }
+}
