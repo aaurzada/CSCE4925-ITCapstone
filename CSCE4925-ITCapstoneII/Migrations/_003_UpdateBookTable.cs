8941dd9fc1463805fc3af5189ad344d865d71283
Andrew Aurzada <aaurzada@gmail.com>
2015-03-12 11:30:26 -0500

    Updated Book and User classes to reflect changes to the book table.  Book table has been changed with the following: courseSection (int --> string); edition (string --> int); isRequired (int --> bool)

---
+++ CSCE4925-ITCapstoneII/Migrations/_003_UpdateBookTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentMigrator;
+
+namespace SQLSolutions.Migrations
+{
+    [Migration(3)]
+    public class _003_UpdateBookTable : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("book")
+                .AlterColumn("edition").AsInt16();
+        }
+
+        public override void Down()
+        {
+            Alter.Table("book")
+                .AlterColumn("edition").AsString();
+        }
+    }
+}
