fe582df2d6f866d2238bf941d322e4dd8f949373
spencernewell93 <spencernewell93@gmail.com>
2015-04-23 10:11:54 -0500

    deleted my nonadmin view User

--- CSCE4925-ITCapstoneII/Controllers/HomeController.cs
+++ CSCE4925-ITCapstoneII/Controllers/HomeController.cs
@@ -65,8 +65,8 @@ namespace SQLSolutions.Controllers
             { 
                 var connect = new LDAPConnect();
                 //CONNECT TO LDAP 
-        //      if (connect.useLDAP("uid=" + _username + ",ou=people,o=unt", _password))
-          //      {
+                if (connect.useLDAP("uid=" + _username + ",ou=people,o=unt", _password))
+                {
                         bool boolIsAdmin = false;
                      
                 
@@ -79,7 +79,7 @@ namespace SQLSolutions.Controllers
                         else
                             isAdmin = false;
                         accountExists = true; //account exists in User table 
-                //}        
+                }        
             }
             if (accountExists == true && isAdmin == true) //if account exists then return admin, nonadmin, or nonexists to homeController
             {
